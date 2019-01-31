95052dca1389648457e04475181d60ec324a3659
Michael <mik@use.startmail.com>
2019-02-01 07:27:38 +1000

    Initial Commit

---
+++ sphData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sphData
+//
+//  Created by Michael Schembri on 1/2/19.
+//  Copyright © 2019 Michael Schembri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
