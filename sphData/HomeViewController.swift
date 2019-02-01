4b16b6333ff5616d5b5bd0a0e6145e19081f334e
Michael <mik@use.startmail.com>
2019-02-01 19:17:52 +1000

    create models from network fetch

--- sphData/HomeViewController.swift
+++ sphData/HomeViewController.swift
@@ -13,11 +13,38 @@ class HomeViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		setUpController()
+		fetchFromNetwork()
 	}
 	
 	func setUpController() {
 		title = "Mobile Data Usage"
 		view.backgroundColor = Theme.current.backgroundColor
 	}
-}
+	
+	func fetchFromNetwork() {
+		// test URL
+		let testUrl = "https://data.gov.sg/api/action/datastore_search?resource_id=a807b7ab-6cad-4aa6-87d0-e283a7353a0f&limit=5"
+		//let testUrl = "https://data.gov.sg/api/action/datastore_search?resource_id=a807b7ab-6cad-4aa6-87d0-e283a7353a0f&q=jones"
 
+		// test fetch data
+		let decoder = JSONDecoder()
+		decoder.dateDecodingStrategy = .iso8601
+		
+		guard let url = URL(string: testUrl) else {
+			fatalError("Invalid URL passed.")
+		}
+		
+		DispatchQueue.global().async {
+			do {
+				let data = try Data(contentsOf: url)
+				let downloaded = try decoder.decode(MobileData.self, from: data)
+				
+				DispatchQueue.main.async {
+					print(downloaded)
+				}
+			} catch {
+				print(error.localizedDescription)
+			}
+		}
+	}
+}
