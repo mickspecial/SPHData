4015835f2214a81f89034b61e566ad8e387e3451
Michael <mik@use.startmail.com>
2019-02-01 20:42:54 +1000

    fix tests as using closures now

--- sphDataTests/sphDataTests.swift
+++ sphDataTests/sphDataTests.swift
@@ -12,20 +12,38 @@ import XCTest
 class sphDataTests: XCTestCase {
 
     func testCreateModelFromLocalFilesEmpty() {
+		let e = expectation(description: "callback closure")
+
 		let decoder = JSONDecoder()
 		decoder.dateDecodingStrategy = .iso8601
 		decoder.decode(MobileData.self, fromFile: "dataSet1") { testData in
 			XCTAssertTrue(testData.success)
 			XCTAssertTrue(testData.records.isEmpty)
+			e.fulfill()
+		}
+		
+		waitForExpectations(timeout: 1) { error in
+			if let error = error {
+				XCTFail("waitForExpectationsWithTimeout errored: \(error)")
+			}
 		}
     }
-	
+
 	func testCreateModelFromLocalFilesNonEmpty() {
+		let e = expectation(description: "callback closure")
+
 		let decoder = JSONDecoder()
 		decoder.dateDecodingStrategy = .iso8601
 		decoder.decode(MobileData.self, fromFile: "dataSet2") { testData in
 			XCTAssertTrue(testData.success)
 			XCTAssertEqual(testData.records.count, 5)
+			e.fulfill()
+		}
+		
+		waitForExpectations(timeout: 1) { error in
+			if let error = error {
+				XCTFail("waitForExpectationsWithTimeout errored: \(error)")
+			}
 		}
 	}
 }
