fd4fa6e3a1db4ef5abf12e1ad2ac78bf6331c765
Michael <mik@use.startmail.com>
2019-02-03 09:43:18 +1000

    minor clean up

--- sphData/Model/MobileData.swift
+++ sphData/Model/MobileData.swift
@@ -11,29 +11,13 @@ import Foundation
 struct MobileData: Codable {
 	let success: Bool
 	let records: [Record]
-	
-	init(from decoder: Decoder) throws {
-		let container = try decoder.container(keyedBy: CodingKeys.self)
-		success = try container.decode(Bool.self, forKey: .success)
-		let result = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .result)
-		records = try result.decode([Record].self, forKey: .records)
-	}
 
-	func encode(to encoder: Encoder) throws {
-		var container = encoder.container(keyedBy: CodingKeys.self)
-		try container.encode(success, forKey: .success)
-		var result = container.nestedContainer(keyedBy: CodingKeys.self, forKey: .result)
-		try result.encode(records, forKey: .records)
-	}
-}
-
-extension MobileData {
 	// group all records with same year into dic
 	// ie 2004: [Record], 2005: [Record] etc
 	private func recordsByYear() -> [String: [Record]] {
 		return Dictionary(grouping: records) { $0.year }
 	}
-	
+
 	func yearlyDataRecords() -> [YearRecord] {
 		let groupedByYear = recordsByYear()
 		let tempData = groupedByYear.values.compactMap { YearRecord(group: $0) }
@@ -46,5 +30,19 @@ extension MobileData {
 		case success
 		case result
 		case records
+	}
+
+	init(from decoder: Decoder) throws {
+		let container = try decoder.container(keyedBy: CodingKeys.self)
+		success = try container.decode(Bool.self, forKey: .success)
+		let result = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .result)
+		records = try result.decode([Record].self, forKey: .records)
+	}
+
+	func encode(to encoder: Encoder) throws {
+		var container = encoder.container(keyedBy: CodingKeys.self)
+		try container.encode(success, forKey: .success)
+		var result = container.nestedContainer(keyedBy: CodingKeys.self, forKey: .result)
+		try result.encode(records, forKey: .records)
 	}
 }
